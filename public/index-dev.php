a532b9f6b5c8e5a32d35f8bcb1330f847eed1bd6
pedro.miranda <pedro.miranda@basis.com.br>
2018-03-20 18:35:11 -0300

    mudança da versão do bootstrap para a 3.3.7 e inclusão da biblioteca jquery 3.3.1

---
+++ public/index-dev.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 04325469109
+ * Date: 20/03/2018
+ * Time: 17:42
+ */
+
+// configuração das 'libraries' da aplicação.
+$msg = "Bem vindo ao sistema de gestão de serviços técnicos de desenvolvimento de sistemas e informática.";
+echo $msg;
