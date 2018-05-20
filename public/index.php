7e54fa3867f6b2b78ecefd3d2f5adba0263e0654
pedro.miranda <pehhagah.1607@gmail.com>
2018-05-20 00:49:44 -0300

    atualização no arquivo principal da aplicação.

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 04325469109
+ * Date: 20/03/2018
+ * Time: 17:42
+ */
+
+// configuração das 'libraries' da aplicação.
+$msg = "SERVICETEC - Setor de Design, Comunicação, Fábrica de Software e Infraestrutura";
+echo $msg;
